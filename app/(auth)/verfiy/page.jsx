9fa1ab31c57b35808ad9532339235efb3613880a
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-08-27 07:44:21 +0300

    added supabase

---
+++ app/(auth)/verfiy/page.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Page() {
+    return (
+        <main className="text-center">
+            <h2>Thanks for registering!</h2>
+            <p>Before logging in, you need to verify your email address.</p>
+        </main>
+    )
+}
+
+
